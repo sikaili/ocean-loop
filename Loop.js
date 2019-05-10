8453054921d28c4279d771f55daac6cd85f62d39
sikai <skyl@me.com>
2019-05-10 22:57:49 +0200

    forme with noise

--- Loop.js
+++ Loop.js
@@ -3,6 +3,7 @@ function Loop(r, x, y) {
   this.pos = createVector(x, y);
   this.r = r;
   this.clock = random(255);
+  this.clock1 = JSON.parse(JSON.stringify(this.clock));
 
   this.display = () => {
     fill(0, 0);
@@ -11,10 +12,10 @@ function Loop(r, x, y) {
     translate(this.pos.x, this.pos.y)
     beginShape()
     for (let i = 0; i < PI * 2; i += 0.04) {
-      this.clock += 0.0001 + this.clock / 20000;
+      this.clock += 0.0001 + this.clock / 10000;
       // this.r = map(noise(this.clock), 0, 1, 0.9999, 1.0001) * this.r;
-      rotate(10 / (noise(this.clock) + 10))
-      let x = (this.r + tan(i) * 10 * sin(frameCount / 20 + this.clock)) * cos(i);
+      rotate(10 / (noise(this.clock) + 10) + this.clock1)
+      let x = (i / (2 * PI) * this.clock1 / 100 + noise(i / (2 * PI)) * this.r + tan(i) * 10 * sin(frameCount / 20 + this.clock)) * cos(i);
       let y = this.r * sin(i)
       vertex(x, y);
     }
@@ -28,7 +29,8 @@ function Loop(r, x, y) {
         let distance = p5.Vector.dist(this.pos, array[i].pos);
         if (distance < (this.r + array[i].r)) {
           this.coli += 1;
-          this.clock += this.clock > 8000 * (this.x / width) ? 0.01 : -0.01;
+          let d = this.clock > this.clock1 * 500 ? -0.01 : -0.3;
+          this.clock += d;
         }
       }
     }
