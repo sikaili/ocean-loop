db5f1b81979f603fd1071ec8e0a24cf1d306feee
sikai <skyl@me.com>
2019-05-25 11:16:26 +0200

    delete

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ let r = 10;
 
 function setup() {
   pixelDensity(1);
-  cvs = createCanvas(windowWidth, windowWidth / 16 * 9);
+  cvs = createCanvas(windowWidth, windowHeight);
   cvs.parent('sketch-holder');
   btn = document.getElementById('record');
   btn.textContent = "start recording";
@@ -49,39 +49,39 @@ function draw() {
   fill(0, 30);
   ellipse(mouseX, mouseY, calR(state, 7 / 1024 * width));
 
-  if (capturer) {
-    capturer.capture(document.getElementById('defaultCanvas0'));
-    if (counter == 8) {
-      frameRate(0);
-      btn.click();
-    }
-  }
+  // if (capturer) {
+  //   capturer.capture(document.getElementById('defaultCanvas0'));
+  //   if (counter == 8) {
+  //     frameRate(0);
+  //     btn.click();
+  //   }
+  // }
 
 }
 
-function record() {
-  capturer = new CCapture({
-    format: 'webm',
-    framerate: 30
-  });
-  capturer.start();
-  btn.textContent = 'stop recording';
+// function record() {
+//   capturer = new CCapture({
+//     format: 'webm',
+//     framerate: 30
+//   });
+//   capturer.start();
+//   btn.textContent = 'stop recording';
 
-  btn.onclick = e => {
-    capturer.stop();
-    capturer.save();
-    capturer = null;
-    btn.textContent = 'start recording';
-    btn.onclick = record;
-  };
-  window.onkeypress = e => {
-    capturer.stop();
-    capturer.save();
-    capturer = null;
-    btn.textContent = 'start recording';
-    btn.onclick = record;
-  };
-}
+//   btn.onclick = e => {
+//     capturer.stop();
+//     capturer.save();
+//     capturer = null;
+//     btn.textContent = 'start recording';
+//     btn.onclick = record;
+//   };
+//   window.onkeypress = e => {
+//     capturer.stop();
+//     capturer.save();
+//     capturer = null;
+//     btn.textContent = 'start recording';
+//     btn.onclick = record;
+//   };
+// }
 
 function createLoop(x, y, _r) {
   _r > 50 ? background(random(100), 0, random(100), r * 4) : "";
