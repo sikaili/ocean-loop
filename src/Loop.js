aa1d8ef0117006222def31d8cf70875810061207
sikai <skyl@me.com>
2019-05-27 16:12:38 +0200

    manuel_to_test

---
+++ src/Loop.js
@@ -0,0 +1,65 @@
+class Loop {
+  constructor(r, x, y) {
+    this.coli = [];
+    this.pos = createVector(x, y);
+    this.r = r;
+    this.clock = random(140, 240);
+    this.clock1 = JSON.parse(JSON.stringify(this.clock));
+    this.go = createVector(random(-3, 3), random(-3, 3));
+    this.start = JSON.parse(JSON.stringify(millis()));
+  }
+  update = () => {
+    this.clock1 < 170 || this.coli.length > 10 || millis() - this.start > 10000 ? this.pos.add(this.go) : "";
+  }
+  display = (array) => {
+    push();
+    fill(0, 0);
+    stroke((this.coli.length % 3 === 1 && this.clock1 > 30) || this.coli.mouse ? [random(0, 255), random(0, 255), random(0, 255)] : 255);
+    beginShape();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.clock1);
+    for (let i = 0; i < PI * 2; i += 0.07) {
+      this.clock += 0.0002;
+      this.clock1 > 180 ? rotate((noise(this.clock) / 10) + this.clock1 / 3) : "";
+      this.clock1 < 160 && Math.random() > 0.5 ? this.r += 0.08 : '';
+      let x = spinningPlate(this.r, i, this.clock1, this.clock, array.length + this.coli.length);
+      let y = this.r * sin(i);
+      this.clock1 < 180 && this.clock1 > 30 ? vertex(y, x) : '';
+      strokeWeight((2 / 1000 * (width + height) / 2 * this.clock1 / 255) * pixelDensity() ^ 2 * map(this.clock1, 140, 240, 0.7, 1.3));
+      point(x + i, y + i);
+      // this.coli.mouse && this.r < 30 ? vertex(x * noise(i), y) : "";
+    }
+    endShape()
+    pop();
+  }
+
+  inter = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      if (this != array[i]) {
+        let distance = p5.Vector.dist(this.pos, array[i].pos);
+        this.coli.mouse = (p5.Vector.dist(this.pos, createVector(mouseX, mouseY)) < this.r);
+        if (distance < (this.r + array[i].r) && this.coli.indexOf(array[i]) == -1 && distance > 2) {
+          this.coli.push(array[i]);
+          this.go.mult(-1);
+          let d = this.clock > this.clock1 * 500 ? -10000 : 0.1;
+        }
+      }
+    }
+  }
+}
+
+function spinningPlate(r, i, clock1, clock, length) {
+  let n = (Math.floor(clock1 + length / 2) % 5);
+  switch (n) {
+    // 圆
+    case 0:
+      return i / (2 * PI) * clock1 / 5 + 1 * r * sin(frameCount / 40 * length + clock) * cos(i)
+    case 1:
+      return i / (2 * PI) * clock1 / 2 + 1 * r * cos(frameCount / 40 + clock) * sin(i)
+      //  蛹
+    case 2:
+      return i / (2 * PI) * clock1 / 5 + noise(clock) * r * sin(frameCount / 40 + clock) * cos(i)
+    case 3:
+      return noise(i) * r * sin(frameCount / 40 + clock) * cos(i)
+  }
+}
--- src/sketch.js
+++ src/sketch.js
@@ -10,57 +10,37 @@ document.addEventListener(
   }
 );
 
-let capturer;
-let btn;
+let btn, cvs;
 let counter = 1;
 let state = -1;
 let doubleClick, ts = [];
 let mic, osc, filt;
 let red;
 let loops = [];
-let r = 30;
-let songs = [],
-  reverb;
-let amplitudes = [];
-
-function preload() {
-  Array(70).fill('').map((a, i) => {
-    songs[i] = loadSound(`assets/horror/sound${i%5}.wav`);
-    amplitudes[i] = new p5.Amplitude();
-  })
-}
+let r = 10;
 
 function setup() {
-  // pixelDensity(1)
+  frameRate(30);
+  console.log(pixelDensity())
+  // pixelDensity(1);
   cvs = createCanvas(windowWidth, windowHeight);
   cvs.parent('sketch-holder');
-  btn = document.getElementById('record');
-  btn.textContent = "start recording";
-  document.body.appendChild(btn);
-  reverb = new p5.Reverb();
-
-  btn.onclick = record;
-  // let m = setInterval(() => {
-  //   createLoop(random(0, width), random(0, height), random(0, 0.1 * (width + height)));
-  // }, 6000);
+  // btn = document.getElementById('record');
+  // btn.textContent = "start recording";
+  // document.body.appendChild(btn);
+  // btn.onclick = record;
+  let m = setInterval(() => {
+    createLoop(random(0, width), random(0, height), random(0, 0.1 * (width + height) / 2));
+  }, 8000);
   // clearInterval(m);
-  // masterVolume(0.1, 3, 3)
-  songs.map((a, i) => {
-    Math.random() > 0.6 ? a.reverseBuffer() : reverb.process(a);
-    a.play();
-    a.playMode('sustain');
-    a.setVolume(0);
-    a.connect();
-    a.stop();
-    amplitudes[i].setInput(a);
-  })
-  reverb.amp(3);
 }
 
 function draw() {
-  background(0, 35);
+
+  background(0, 50);
   let r = 100;
-  loops.map((a, i) => {
+
+  loops.map(a => {
     a.update();
     if (a.r < (width + height) / 2 / 2 && (a.pos.x > 0 && a.pos.x < width && a.pos.y > 0 && a.pos.y < height)) {
       if (((a.coli.length % 3 === 1 && a.clock1 > 30) || a.coli.mouse) && !songs[i].isPlaying()) {
@@ -98,39 +78,40 @@ function draw() {
   }
 }
 
-function record() {
-  capturer = new CCapture({
-    format: 'webm',
-    framerate: 30
-  });
-  capturer.start();
-  btn.textContent = 'stop recording';
 
-  btn.onclick = e => {
-    capturer.stop();
-    capturer.save();
-    capturer = null;
-    btn.textContent = 'start recording';
-    btn.onclick = record;
-  };
-  window.onkeypress = e => {
-    capturer.stop();
-    capturer.save();
-    capturer = null;
-    btn.textContent = 'start recording';
-    btn.onclick = record;
-  };
-}
+
+// function record() {
+//   capturer = new CCapture({
+//     format: 'webm',
+//     framerate: 30
+//   });
+//   capturer.start();
+//   btn.textContent = 'stop recording';
+
+//   btn.onclick = e => {
+//     capturer.stop();
+//     capturer.save();
+//     capturer = null;
+//     btn.textContent = 'start recording';
+//     btn.onclick = record;
+//   };
+//   window.onkeypress = e => {
+//     capturer.stop();
+//     capturer.save();
+//     capturer = null;
+//     btn.textContent = 'start recording';
+//     btn.onclick = record;
+//   };
+// }
 
 function createLoop(x, y, _r) {
-  console.log(loops.length)
   _r > 50 ? background(random(100), 0, random(100), r * 4) : "";
-  // loops.length > 70 ? Math.random() > 0.1 ? loops.splice(0, 15) : loops.splice(0, 65) : "";
+  loops = loops.filter(a => a.r < width / 3 && a.pos.x > 0);
+  loops.length > 100 ? Math.random() > 0.1 ? loops.splice(0, 15) : loops.splice(0, 70) : "";
   let num = Math.floor(Math.random() * 10);
-  // num = 1;
   y > height ? num = 0 : '';
   for (let i = 0; i < num; i++) {
-    let dump = new Loop(_r + random(20), x, y);
+    let dump = new Loop(_r + random(-10, 10), x, y);
     loops.push(dump);
   }
   loops.length > 70 ?
