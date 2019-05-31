bd4e6cc659f60a3f851e2db3720aa6723c91d78c
sikai <skyl@me.com>
2019-05-31 18:09:53 +0200

    ajust speed spinning

--- src/Loop.js
+++ src/Loop.js
@@ -14,44 +14,44 @@ class Loop {
   update = () => {
     this.shrink ? (() => {
       this.coli.length = 0;
-      this.r > 0 ? this.r -= 1 : "";
+      this.r > 0.9 ? this.r -= 1 : "";
       this.coli.mouse = false;
     })() : "";
     this.bigger ? (() => {
       this.r += 2;
     })() : '';
-    this.clock1 < 170 || this.coli.length > 10 || millis() - this.start > 7000 ? this.pos.add(this.go) : "";
-    // this.coli.mouse && this.r - this.rInit < 100 ? this.r += 0.1 : this.r -= this.r > this.rInit ? 0.1 : 0;
+    this.clock1 < 170 || this.coli.length > 13 || millis() - this.start > 7000 ? this.pos.add(this.go) : "";
   }
   display = (array, amp) => {
     push();
-    fill(0, 0);
+    this.r < width / 6 ? fill(255, 1) : noFill();
     if ((this.coli.length % 3 === 1 && this.clock1 > 160) || this.coli.mouse) {
       stroke([random(0, 255), random(0, 255), random(0, 255), (this.clock1 / 1.5 + 30 + amp * 150)]);
     } else {
-      stroke([255, this.r / 4 + amp * 150 + 20]);
+      stroke([255, this.r / 2 + amp * 150 + 20]);
     }
     beginShape();
     translate(this.pos.x, this.pos.y);
-    push();
-    stroke(255, 30 + amp * 150);
+    // push();
+    // stroke(255, 30 + amp * 150);
     // text(amp.toString().slice(0, 5), 0, this.r);
-    pop();
+    // pop();
     // this.r = this.rInit * (this.coli.length + 1) / 2;
-    rotate(this.clock1);
+    // rotate(this.clock1);
     for (let i = 0; i < PI * 2; i += 0.07) {
-      this.clock += 0.0002;
-      this.clock1 > 180 ? rotate((noise(this.clock) / 10) + this.clock1 / 3) : "";
-      this.clock1 < 160 && Math.random() > 0.5 ? this.r += 0.05 : '';
+      this.clock += 0.0002 * this.clock1 / 200;
+      this.clock1 > 180 ? rotate((noise(this.clock / 5, i) / 10) + this.clock1 / 3) : "";
+      this.clock1 < 170 && Math.random() > 0.5 ? this.r += 0.1 : '';
       let x = spinningPlate(this.r, i, this.clock1, this.clock, array.length + this.coli.length, amp) * (this.clock1 < 220 ? constrain(map(amp, 0, 0.08, 1.3, 0.3), 1.3, 0.3) : 1);
       let y = this.r * sin(i);
       this.clock1 < 150 && noise(this.clock, i) > 0.8 ? vertex(y, x) : "";
+      abs(x) > width / 3 ? strokeWeight(noise(x / 100, i) * 3) : "";
+
       strokeWeight((1.5 / 1000 * (width + height) / 2 * this.clock1 / 255) * pixelDensity() ^ 2 / 1.5 * map(this.clock1, 140, 240, 0.7, 1.3));
-      abs(x) > width / 3 ? strokeWeight(noise(x) * 3) : "";
       point(x + i, y + i);
-      Math.random() > 0.2 ? strokeWeight(1) : '';
       this.clock1 > 200 ? vertex(y + noise(i, this.clock) * 5, x + noise(i) * 100) : "";
     }
+    Math.random() > 0.2 ? strokeWeight(1) : '';
     endShape()
     pop();
   }
